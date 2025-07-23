4798ce3ac60fb4d3a679c774ace212e1d306cacd
RommelJakeGMerca <rommeljake@gmail.com>
2025-07-23 09:29:54 +0800

    #SP1TS-5 | REGISTRATION SYSTEM - Local Project Setup, Setting up project to local machine

--- src/main/kotlin/configuration/Config.kt
+++ src/main/kotlin/configuration/Config.kt
@@ -8,17 +8,15 @@ import java.sql.DriverManager
 class Config {
     //TODO: HikariCP and pgBouncer
     fun connect(): Connection {
-        val dotenv = dotenv()
+        val dotenv   = dotenv()
         val database = dotenv["DB_DATABASE"]
         val hostname = dotenv["DB_HOSTNAME"]
-        val port = dotenv["DB_PORT"]
-        val dbName = dotenv["DB_NAME"]
-        val url = "jdbc:$database://$hostname:$port/$dbName"
-//
+        val port     = dotenv["DB_PORT"]
+        val dbName   = dotenv["DB_NAME"]
+        val url      = "jdbc:$database://$hostname:$port/$dbName"
         val username = dotenv["DB_USERNAME"]
         val password = dotenv["DB_PASSWORD"]
 
-        println("Connecting to postgres database at $url, with username $username and password $password")
         return DriverManager.getConnection(url, username, password)
     }
 }
