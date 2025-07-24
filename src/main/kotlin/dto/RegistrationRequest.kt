0362cae690d0ab352c82b3a811126d23c6b1c270
Rommel Merca <rommel.merca@umtc.com.ph>
2025-07-24 17:08:25 +0800

    #SP1TS-16 | REGISTRATION SYSTEM, creation of the RegistrationRequest DTO for request handling

---
+++ src/main/kotlin/dto/RegistrationRequest.kt
@@ -0,0 +1,14 @@
+package com.marlow.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RegistrationRequest(
+    val username: String,
+    val firstName: String,
+    val middleName: String? = null,
+    val lastName: String? = null,
+    val email: String,
+    val password: String,
+    val birthday: String? = null
+)
