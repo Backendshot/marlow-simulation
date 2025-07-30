480d41824fe9a821490de3c031b146afe1380afe
DeltaCream <81692797+DeltaCream@users.noreply.github.com>
2025-07-30 13:21:19 +0800

    Update RouteExtensions.kt
    optimized imports for RouteExtensions

--- src/main/kotlin/todo/route/RouteExtensions.kt
+++ src/main/kotlin/todo/route/RouteExtensions.kt
@@ -1,17 +1,7 @@
 package com.marlow.todo.route
 
-import com.marlow.global.GlobalResponse
-import com.marlow.global.GlobalResponseData
-import com.marlow.todo.model.Todo
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.plugins.*
-import io.ktor.server.response.*
-import io.ktor.http.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.decodeFromJsonElement
-import kotlinx.serialization.json.jsonObject
 
 // 1. Parameter extractor
 fun ApplicationCall.requireIntParam(name: String): Int =
