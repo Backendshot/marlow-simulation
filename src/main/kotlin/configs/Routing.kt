9f1314b010a2f4af32e896a4591829993155a211
Rommel Merca <rommel.merca@umtc.com.ph>
2025-08-04 16:10:38 +0800

    adding registration documentation

--- src/main/kotlin/configs/Routing.kt
+++ src/main/kotlin/configs/Routing.kt
@@ -13,7 +13,9 @@ import registrationRouting
 
 fun Application.configureRouting(ds: HikariDataSource) {
     routing {
-        swaggerUI("/swagger", "src/main/kotlin/systems/todo/documentation/documentation.yaml")
+        swaggerUI("/swagger/loginSystem", "src/main/kotlin/systems/login/documentation/documentation.yaml")
+        swaggerUI("/swagger/registrationSystem", "src/main/kotlin/systems/registration/documentation/documentation.yaml")
+        swaggerUI("/swagger/todoSystem", "src/main/kotlin/systems/todo/documentation/documentation.yaml")
         authenticate("auth-bearer") {
             todoRouting(ds)
         }
